af15d7d3fa94dea878cecee1fd23480bdfc704a9
TharinduDR <rhtdranasinghe@gmail.com>
2026-06-15 21:39:55 +0100

    001: Initial commit

---
+++ hec.extract.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -p gpu-medium
+#SBATCH --gres=gpu:nvidia_h200_nvl:2
+#SBATCH --mem=100G
+#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=t.ranasinghe@lancaster.ac.uk
+
+source /etc/profile
+module add anaconda3/2023.09
+module add cuda/12.0
+
+source activate /storage/hpc/37/ranasint/conda_envs/llm_exp
+export HF_HOME=/scratch/hpc/37/ranasint/hf_cache
+export HF_TOKEN=
+
+python -m batch_extract.py https://aclanthology.org/volumes/2025.acl-long/ --qwen_device cuda:0 --gemma_device cuda:1 --thinking_device auto
+
+
+
